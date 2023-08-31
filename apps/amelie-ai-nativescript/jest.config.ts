a19672e9b08bd229c1e9b59cf0160e6da18c65a8
František Žiačik <frantisek.ziacik@resco.net>
2023-08-31 21:56:56 +0200

    chore: setup of jest testing in the app

--- apps/amelie-ai-nativescript/jest.config.ts
+++ apps/amelie-ai-nativescript/jest.config.ts
@@ -1,16 +1,19 @@
-module.exports = {
+/* eslint-disable */
+export default {
+	displayName: 'amelie-ai-nativescript',
 	preset: '../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-	globals: {
-		'ts-jest': {
-			stringifyContentPathRegex: '\\.(html|svg)$',
-			astTransformers: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer'],
-			tsconfig: '<rootDir>/tsconfig.spec.json',
-		},
-	},
 	coverageDirectory: '../../coverage/apps/amelie-ai-nativescript',
-
-	displayName: 'nativescript-safety',
+	transform: {
+		'^.+\\.(ts|mjs|js|html)$': [
+			'jest-preset-angular',
+			{
+				tsconfig: '<rootDir>/tsconfig.spec.json',
+				stringifyContentPathRegex: '\\.(html|svg)$',
+			},
+		],
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 	snapshotSerializers: [
 		'jest-preset-angular/build/serializers/no-ng-attributes',
 		'jest-preset-angular/build/serializers/ng-snapshot',
