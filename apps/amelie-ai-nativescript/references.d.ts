84d49d82d8686c9c08d28b36ce7d4d368bfb6999
František Žiačik <frantisek.ziacik@resco.net>
2023-08-31 21:04:51 +0200

    chore: config stuff

--- apps/amelie-ai-nativescript/references.d.ts
+++ apps/amelie-ai-nativescript/references.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="../../node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="../../node_modules/@nativescript/types-android/lib/android-21.d.ts" />
+/// <reference path="../../node_modules/@nativescript/types-android/lib/android-23.d.ts" />
