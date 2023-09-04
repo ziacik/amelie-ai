6b03565cb7ec0b45ee20be2ce03c02496442c1fd
František Žiačik <frantisek.ziacik@resco.net>
2023-09-04 19:15:14 +0200

    feat: readability service

--- apps/readability-service-e2e/src/readability-service/readability-service.spec.ts
+++ apps/readability-service-e2e/src/readability-service/readability-service.spec.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-describe('GET /', () => {
-	it('should return a message', async () => {
-		const res = await axios.get(`/`);
-
+describe('GET /readable', () => {
+	it('fetches the specified url and simplifies the content', async () => {
+		const res = await axios.get(`/article?url=http://localhost:3000/__test__/test.html`);
 		expect(res.status).toBe(200);
-		expect(res.data).toEqual({ message: 'Hello API' });
+		expect(res.data).toMatchSnapshot();
 	});
 });
--- apps/readability-service/src/main.ts
+++ apps/readability-service/src/main.ts
@@ -1,13 +1,60 @@
+import { Readability } from '@mozilla/readability';
+import axios from 'axios';
 import express from 'express';
+import { readFileSync } from 'fs';
+import { JSDOM } from 'jsdom';
+import { resolve } from 'path';
+import { env } from 'process';
+import TurndownService from 'turndown';
 
-const host = process.env['HOST'] ?? 'localhost';
-const port = process.env['PORT'] ? Number(process.env['PORT']) : 3000;
+import DOMPurify from 'dompurify';
+
+const host = env['HOST'] ?? 'localhost';
+const port = env['PORT'] ? Number(process.env['PORT']) : 3000;
+const isDevelopment = env['NODE_ENV'] === 'development';
 
 const app = express();
+const turndownService = new TurndownService({ headingStyle: 'atx' });
 
-app.get('/', (req, res) => {
-	res.send({ message: 'Hello API' });
+app.get('/article', async (req, res) => {
+	const url: string | undefined = req.query['url']?.toString();
+
+	if (!url) {
+		res.status(400).send('Param required: url.');
+		return;
+	}
+
+	const fetchResponse = await axios.get(url);
+	const html: string = fetchResponse.data;
+
+	const dom = new JSDOM(html);
+	const purify = DOMPurify(dom.window);
+	const readability = new Readability(dom.window.document);
+	const readable = readability.parse();
+
+	if (!readable) {
+		res.status(500).send('Unable to make the article readable.');
+		return;
+	}
+
+	const purified = purify.sanitize(readable.content, { ALLOWED_TAGS: ['p', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'title'] });
+	const content = turndownService.turndown(purified);
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const { textContent: _1, content: _2, ...readableWithoutContent } = readable;
+
+	res.send({
+		...readableWithoutContent,
+		content,
+	});
 });
+
+if (isDevelopment) {
+	app.get('/__test__/test.html', (req, res) => {
+		const text = readFileSync(resolve(__dirname, '../../../../../../apps/readability-service-e2e/__fixture__/Amélia.html'), 'utf8');
+		res.send(text);
+	});
+}
 
 app.listen(port, host, () => {
 	console.log(`[ ready ] http://${host}:${port}`);
