c3ecce567491d55ec908fcc38f3abc714bf07557
František Žiačik <ziacik@gmail.com>
2023-09-06 19:23:38 +0200

    feat: readability service as azure func

--- apps/readability-service/src/main.ts
+++ apps/readability-service/src/main.ts
@@ -1,27 +1,22 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
 import { Readability } from '@mozilla/readability';
 import axios from 'axios';
-import express from 'express';
-import { readFileSync } from 'fs';
+import DOMPurify from 'dompurify';
 import { JSDOM } from 'jsdom';
-import { resolve } from 'path';
-import { env } from 'process';
 import TurndownService from 'turndown';
 
-import DOMPurify from 'dompurify';
+export async function article(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+	context.log(`Http function processed request for url "${request.url}"`);
 
-const host = env['HOST'] ?? 'localhost';
-const port = env['PORT'] ? Number(process.env['PORT']) : 3000;
-const isDevelopment = env['NODE_ENV'] === 'development';
-
-const app = express();
-const turndownService = new TurndownService({ headingStyle: 'atx' });
-
-app.get('/article', async (req, res) => {
-	const url: string | undefined = req.query['url']?.toString();
+	const url: string | undefined = request.query.get('url')?.toString();
 
 	if (!url) {
-		res.status(400).send('Param required: url.');
-		return;
+		return {
+			status: 400,
+			jsonBody: {
+				error: 'Param required: url.',
+			},
+		};
 	}
 
 	const fetchResponse = await axios.get(url);
@@ -33,29 +28,41 @@ app.get('/article', async (req, res) => {
 	const readable = readability.parse();
 
 	if (!readable) {
-		res.status(500).send('Unable to make the article readable.');
-		return;
+		return {
+			status: 500,
+			jsonBody: {
+				error: 'Unable to make the article readable.',
+			},
+		};
 	}
 
 	const purified = purify.sanitize(readable.content, { ALLOWED_TAGS: ['p', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'title'] });
+
+	const turndownService = new TurndownService({ headingStyle: 'atx' });
 	const content = turndownService.turndown(purified);
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { textContent: _1, content: _2, ...readableWithoutContent } = readable;
 
-	res.send({
+	const result = {
 		...readableWithoutContent,
 		content,
-	});
-});
+	};
 
-if (isDevelopment) {
-	app.get('/__test__/test.html', (req, res) => {
-		const text = readFileSync(resolve(__dirname, '../../../../../../apps/readability-service-e2e/__fixture__/Amélia.html'), 'utf8');
-		res.send(text);
-	});
+	return { body: JSON.stringify(result) };
 }
 
-app.listen(port, host, () => {
-	console.log(`[ ready ] http://${host}:${port}`);
+app.http('article', {
+	methods: ['GET'],
+	authLevel: 'anonymous',
+	handler: article,
 });
+
+// todo
+
+// if (isDevelopment) {
+// 	app.get('/__test__/test.html', (req, res) => {
+// 		const text = readFileSync(resolve(__dirname, '../../../../../../apps/readability-service-e2e/__fixture__/Amélia.html'), 'utf8');
+// 		res.send(text);
+// 	});
+// }
